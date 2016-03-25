29ace6f4680d67c0bbff54a849cbf2f273010a10
lamchristine <christine.lam.06@gmail.com>
2016-03-25 00:00:59 -0700

    dot moves

---
+++ app.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+
+  // var countS = 0;
+
+
+
+  $(document).on("keyup", function (key){
+    if (key.keyCode === 65) { //'a'
+      $("#red").animate({ "left": "+=50px" }, "slow" )
+      }
+    });
+  //
+  //   // $(".board1.cell1").append("<img src='http://maps.google.com/mapfiles/ms/icons/green-dot.png'>")
+  //       if ( (countS % 2 === 0) && (key.keyCode === 65) ) { //'a'
+  //         $(".board1.cell").append("<img src='http://maps.google.com/mapfiles/ms/icons/green-dot.png'>");
+  //         $(".board1").addClass("cell" + countS);
+  //         countS = countS + 1;
+  //         // $(".board1.cell(countS)").append("<img src='http://maps.google.com/mapfiles/ms/icons/green-dot.png'>");
+  //
+  //       //
+  //       } else if ( (countS % 2 !== 0) && (key.keyCode === 65) ) {
+  //         $(".board1").empty();
+  //
+  //         $(".board1").addClass("<img src='http://maps.google.com/mapfiles/ms/icons/green-dot.png'>");
+  //         countS = countS + 1;
+  //
+  //   // }      // }
+  // });
+});
