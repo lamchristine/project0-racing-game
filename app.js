0b15271e46a408d0151edfaf886e36c7d0a87076
lamchristine <christine.lam.06@gmail.com>
2016-03-26 22:08:05 -0700

    added reset

--- app.js
+++ app.js
@@ -3,16 +3,40 @@
 $(document).ready(function() {
 
   var winner;
-  var player1 = "Red";
-  var player2 = "Blue";
+  var player1;
+  var player2;
   var eachMove = 20;
 
 
   var position1 = $('#red').position();
+  console.log(position1)
   var position2 = $('#blue').position();
+  console.log(position2.left)
   var finishLine1 = 0.85 * $('#board1').width();
-  var finishLine2 = $('#board2').width();
-  // var positionBoard1 = $('#board1').position();
+  var finishLine2 = 0.85 * $('#board2').width();
+
+  function getPlayer1 () {
+    $(".player1").on("click", function (){
+      player1 = prompt("Please enter your name");
+      $(this).text(player1);
+      return player1;
+    });
+  } getPlayer1();
+
+  function getPlayer2 () {
+    $(".player2").on("click", function (){
+      player2 = prompt("Please enter your name");
+      $(this).text(player2);
+      return player2;
+    });
+  } getPlayer2();
+
+  function reset() {
+    $(".reset").on("click", function() {
+      $("#red").css("left","15px");
+      $("#blue").css("left","15px");
+    });
+  } reset();
 
 
   function movePlayer1() {
@@ -20,7 +44,7 @@ $(document).ready(function() {
         $("#red").animate({ "left": "+=20px" }, "fast");
         position1.left += eachMove;
       } else if (position1.left > finishLine1) {
-        winner = "Red";
+        winner = player1;
         alert ("The Winner is " + winner);
       }
     }
@@ -30,14 +54,21 @@ $(document).ready(function() {
         $("#blue").animate({ "left": "+=20px" }, "fast");
         position2.left += eachMove;
       } else if (position2.left > finishLine2) {
-        winner = "Blue";
+        winner = player2;
         alert ("The Winner is " + winner);
       }
     }
 
+  function startGame () {
+    $(".start").on("click", function () {
+      console.log("game start");
+      getWinner();
+    });
+  } startGame();
+
   function getWinner() {
     $(document).on("keyup", function (key) {
-      if (winner !== "Red" && winner !== "Blue" ) {
+      if (winner !== player1 && winner !== player2) {
         if (key.keyCode === 65) {
           movePlayer1();
         } else if (key.keyCode === 76) {
@@ -46,7 +77,5 @@ $(document).ready(function() {
       }
     });
   }
-
-  getWinner();
 
 });
